0ebae5f95e78ce8fac9403f76aaf87820cd9f352
gtgstg007 <gtgstg@gmail.com>
2019-06-08 16:55:22 +0530

    Complete

--- Blockchain.js
+++ Blockchain.js
@@ -1,14 +1,31 @@
 const SHA256=require('crypto-js/sha256');
+class Transaction{
+	constructor(fromAddress,toAddress,amount){
+		this.fromAddress=fromAddress;
+		this.toAddress=toAddress;
+		this.amount=amount;
+	}
+}
 class Block{
-	constructor(index,timestamp,data,previousHash=''){
-		this.index=index;
+	// constructor(index,timestamp,data,previousHash=''){
+	// 	this.index=index;
+	// 	this.timestamp=timestamp;
+	// 	this.previousHash=previousHash;
+	// 	this.hash=this.calculateHash();
+	// 	this.nonce=0;
+	// }
+	// calculateHash(){
+	// 	return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data)+this.nonce).toString();
+	// }
+	constructor(timestamp,transaction,data,previousHash=''){
 		this.timestamp=timestamp;
+		this.transaction=transaction;
 		this.previousHash=previousHash;
 		this.hash=this.calculateHash();
 		this.nonce=0;
 	}
 	calculateHash(){
-		return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data)+this.nonce).toString();
+		return SHA256(this.previousHash+this.timestamp+JSON.stringify(this.data)+this.nonce).toString();
 	}
 	mineBlock(difficulty){
 		while(this.hash.substring(0,difficulty)!=Array(difficulty+1).join("0")){
@@ -22,18 +39,46 @@ class Blockchain{
 	constructor(){
 		this.chain=[this.createGenesisBlock()];
 		this.difficulty=4;
+		this.pendingTransaction=[];
+		this.miningReward=100;
 	}
 	createGenesisBlock(){
-		return new Block(0,"07/06/2019","Genesis Block","0");
+		//return new Block(0,"07/06/2019","Genesis Block","0");
+		return new Block("07/06/2019","Genesis Block","0");
 	}
 	getLatestBlock(){
 		return this.chain[this.chain.length-1];
 	} 
-	addBlock(newBlock){
-		newBlock.previousHash=this.getLatestBlock().hash;
-		//newBlock.hash=newBlock.calculateHash();
-		newBlock.mineBlock(this.difficulty);
-		this.chain.push(newBlock);
+	// addBlock(newBlock){
+	// 	newBlock.previousHash=this.getLatestBlock().hash;
+	// 	//newBlock.hash=newBlock.calculateHash();
+	// 	newBlock.mineBlock(this.difficulty);
+	// 	this.chain.push(newBlock);
+	// }
+	minePendingTransactions(miningRewardAddress){
+		let block =new Block(Date.now(),this.pendingTransaction);
+		block.mineBlock(this.difficulty);
+		this.pendingTransaction=[
+		new Transaction(null,miningRewardAddress,this.miningReward)];
+		console.log('block successfully mined');
+		this.chain.push(block);
+	}
+	createTransaction(transaction){
+		this.pendingTransaction.push(transaction);
+	}
+	getBalanceofAddress(address){
+		let balance=0;
+		for(const block of this.chain){
+			for(const trans of block.transaction){
+				if(trans.fromAddress===address){
+					balance-=trans.amount;
+				}
+				if(trans.toAddress===address){
+					balance+=trans.amount;
+				}
+			}
+		}
+		return balance;
 	}
 	isChainValid(){
 		for(let i=1;i<this.chain.length;i++){
@@ -53,7 +98,15 @@ let ourCrypto=new Blockchain();
 // ourCrypto.chain[1].data={amount:100};
 // ourCrypto.chain[1].hash=ourCrypto.chain[1].calculateHash();
 // console.log('Is Blockchain valid '+ourCrypto.isChainValid());
-console.log("Mining Block 1...");
-ourCrypto.addBlock(new Block(1,"10/01/2019",{amount:5}));
-console.log("Mining Block 2...");
-ourCrypto.addBlock(new Block(2,"01/10/2019",{amount:10}));
+// console.log("Mining Block 1...");
+// ourCrypto.addBlock(new Block(1,"10/01/2019",{amount:5}));
+// console.log("Mining Block 2...");
+// ourCrypto.addBlock(new Block(2,"01/10/2019",{amount:10}));
+ourCrypto.createTransaction(new Transaction('address1','address2',100));
+ourCrypto.createTransaction(new Transaction('address2','address1',50));
+console.log('\nStarting the miner.');
+ourCrypto.minePendingTransactions('our-address');
+console.log('\nBalance of our account is',ourCrypto.getBalanceofAddress('our-address'));
+console.log('\nStarting the miner, a second time');
+ourCrypto.minePendingTransactions('our-address');
+console.log('\nBalance of our account is',ourCrypto.getBalanceofAddress('our-address'));
